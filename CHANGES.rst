e30f1dd4e48e174ef16bedd5e0fe17782259be5d
Tetsu Haruyama <tetsu.yes@gmail.com>
2020-06-10 15:20:21 +0900

    0.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,1 +1,2 @@
+v0.1.1, 2020-06-10 -- ML formulae for Tobit are corrected. APE and PEA for Tobit are deleted.
 v0.1.0, 2020-05-21 -- Initial release.
--- py4etrics/__init__.py
+++ py4etrics/__init__.py
@@ -7,5 +7,5 @@ from py4etrics import *
 __all__ = ['load']
 
 __author__ = 'Tetsu Haruyama'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __copyright__ = 'Copyright (c) 2020 Tetsu Haruyama'
